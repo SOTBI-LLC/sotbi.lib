37d8881c2073df745179d44022bf6c1ac9e8a45c
Rafael Mustafin <rafael@mustafin.su>
2025-05-02 19:23:25 +0300

    shared back

--- scripts/system/gen.mk
+++ scripts/system/gen.mk
@@ -1,4 +1,5 @@
 gen_proto:
+	protoc --go_out=./pkg/ --go_opt=paths=source_relative ./api/notification/*.proto
 	protoc --go_out=./pkg/ --go_opt=paths=source_relative --go-grpc_out=./pkg/ --go-grpc_opt=paths=source_relative api/excel_gen/*.proto
 	protoc --go_out=./pkg/api --go_opt=paths=source_relative --go-grpc_out=./pkg/api --go-grpc_opt=paths=source_relative \
 	--grpc-gateway_out=./pkg/api --grpc-gateway_opt=paths=source_relative -I ./api \
