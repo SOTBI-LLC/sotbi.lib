7e51ad0912df7503dac8708cea9dc0e9d9e822a5
Rafael Mustafin <rafael@mustafin.su>
2025-06-18 02:20:22 +0300

    Changes to be committed: 	modified:   go.mod 	modified:   go.sum 	modified:   pkg/onec/account_balance.go 	modified:   pkg/onec/exchange_file.go

--- pkg/onec/account_balance.go
+++ pkg/onec/account_balance.go
@@ -1,11 +1,11 @@
 package onec
 
 type AccountBalance struct {
-	StartDate      string  `mapstructure:"ДатаНачала,omitempty"       json:"start_date,omitempty"`
-	EndDate        string  `mapstructure:"ДатаКонца,omitempty"        json:"end_date,omitempty"`
-	Account        string  `mapstructure:"РасчСчет,omitempty"         json:"account,omitempty"`
-	InitialBalance float64 `mapstructure:"НачальныйОстаток,omitempty" json:"initial_balance"`
-	Income         float64 `mapstructure:"ВсегоПоступило,omitempty"   json:"income"`
-	WriteOff       float64 `mapstructure:"ВсегоСписано,omitempty"     json:"write_off"`
-	FinalBalance   float64 `mapstructure:"КонечныйОстаток,omitempty"  json:"final_balance"`
+	StartDate      string  `mapstructure:"ДатаНачала"       json:"start_date,omitempty"`
+	EndDate        string  `mapstructure:"ДатаКонца"        json:"end_date,omitempty"`
+	Account        string  `mapstructure:"РасчСчет"         json:"account,omitempty"`
+	InitialBalance float64 `mapstructure:"НачальныйОстаток" json:"initial_balance"`
+	Income         float64 `mapstructure:"ВсегоПоступило"   json:"income"`
+	WriteOff       float64 `mapstructure:"ВсегоСписано"     json:"write_off"`
+	FinalBalance   float64 `mapstructure:"КонечныйОстаток"  json:"final_balance"`
 }
--- pkg/onec/exchange_file.go
+++ pkg/onec/exchange_file.go
@@ -1,13 +1,13 @@
 package onec
 
 type ExchangeFile struct {
-	FormatVer   string   `mapstructure:"ВерсияФормата,omitempty" json:"format_ver"`
-	Encoding    string   `mapstructure:"Кодировка,omitempty"     json:"encoding"`
-	Sender      string   `mapstructure:"Отправитель,omitempty"   json:"sender"`
-	Receiver    string   `mapstructure:"Получатель,omitempty"    json:"receiver"`
-	CreatedDate string   `mapstructure:"ДатаСоздания,omitempty"  json:"created_date,omitempty"`
-	CreatedTime string   `mapstructure:"ВремяСоздания,omitempty" json:"created_time,omitempty"`
-	StartDate   string   `mapstructure:"ДатаНачала,omitempty"    json:"start_date,omitempty"`
-	EndDate     string   `mapstructure:"ДатаКонца,omitempty"     json:"end_date,omitempty"`
-	Account     []string `mapstructure:"РасчСчет,omitempty"      json:"account,omitempty"`
+	FormatVer   string   `mapstructure:"ВерсияФормата"      json:"format_ver"`
+	Encoding    string   `mapstructure:"Кодировка"          json:"encoding"`
+	Sender      string   `mapstructure:"Отправитель"        json:"sender"`
+	Receiver    string   `mapstructure:"Получатель"         json:"receiver"`
+	CreatedDate string   `mapstructure:"ДатаСоздания"       json:"created_date,omitempty"`
+	CreatedTime string   `mapstructure:"ВремяСоздания"      json:"created_time,omitempty"`
+	StartDate   string   `mapstructure:"ДатаНачала"         json:"start_date,omitempty"`
+	EndDate     string   `mapstructure:"ДатаКонца"          json:"end_date,omitempty"`
+	Account     []string `mapstructure:"РасчСчет,omitempty" json:"account,omitempty"`
 }
