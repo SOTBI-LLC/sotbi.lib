970e54408f8f5dbdea1b71a8073d4b6e0969374e
Qussth <qussth@bk.ru>
2025-08-04 20:18:13 +0200

    [https://bazys.atlassian.net/browse/BH-1600]: Add unified GetOriginalFileName method

---
+++ pkg/files.go
@@ -0,0 +1,31 @@
+package pkg
+
+import (
+	"context"
+
+	"gorm.io/gorm"
+
+	"github.com/COTBU/sotbi.lib/pkg/commonqueries"
+)
+
+func GetOriginalFileName(ctx context.Context, db *gorm.DB, file string) (string, error) {
+	var attachment struct {
+		File             string
+		OriginalFileName string
+	}
+
+	err := db.
+		Debug().
+		WithContext(ctx).
+		Table(commonqueries.NamedTable(commonqueries.FileLinks, "file_links")).
+		Where(`"file" = ?`, file).
+		Order("file").
+		Limit(1).
+		Find(&attachment).
+		Error
+	if err != nil {
+		return "", err
+	}
+
+	return attachment.OriginalFileName, nil
+}
