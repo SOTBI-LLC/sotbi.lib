bbb04ac03161a222992b03516739d941e6263b97
Rafael Mustafin <rafael@mustafin.su>
2026-08-30 20:47:17 +0300

    Remove motivation API definitions and generated gRPC code
    - Deleted the Swagger JSON file for the motivation service, which contained API definitions and data models.
    - Removed the generated gRPC code for the motivation service, including client and server interfaces.
    - Updated the Makefile to exclude the generation of motivation service files from the protobuf compilation process.

--- scripts/system/gen.mk
+++ scripts/system/gen.mk
@@ -5,6 +5,3 @@ gen_proto:
 	protoc --go_out=./pkg/api --go_opt=paths=source_relative --go-grpc_out=./pkg/api --go-grpc_opt=paths=source_relative \
 	--grpc-gateway_out=./pkg/api --grpc-gateway_opt=paths=source_relative -I ./api \
 	--openapiv2_out=./pkg/api ./api/counterparty/*.proto
-	protoc --go_out=./pkg/api --go_opt=paths=source_relative --go-grpc_out=./pkg/api --go-grpc_opt=paths=source_relative \
-	--grpc-gateway_out=./pkg/api --grpc-gateway_opt=paths=source_relative -I ./api \
-	--openapiv2_out=./pkg/api ./api/motivation/*.proto
